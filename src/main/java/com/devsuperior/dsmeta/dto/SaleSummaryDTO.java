0a4c2eac8b208e5d6b456aea26cf17611b5da4de
Hennan H. Falcão <hennan.falcao@cs.unipe.edu.br>
2025-08-25 17:21:55 -0300

    SaleReportDTO implemented, SaleSummaryDTO implemented, searchReport query implemented

---
+++ src/main/java/com/devsuperior/dsmeta/dto/SaleSummaryDTO.java
@@ -0,0 +1,32 @@
+package com.devsuperior.dsmeta.dto;
+
+import java.time.LocalDate;
+
+public class SaleSummaryDTO {
+
+    private String sellerName;
+    private Double total;
+
+    public SaleSummaryDTO(String sellerName, Double total) {
+        this.sellerName = sellerName;
+        this.total = total;
+    }
+
+    public SaleSummaryDTO(){}
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    public String getSellerName() {
+        return sellerName;
+    }
+
+    public void setSellerName(String sellerName) {
+        this.sellerName = sellerName;
+    }
+}
